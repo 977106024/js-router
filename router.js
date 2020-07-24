c450136a56a036a46bb74b8c5152593e9f93d833
bohong <977106024@qq.com>
2020-07-24 20:51:22 +0800

    使用函数优化代码

--- router.js
+++ router.js
@@ -1,39 +1,29 @@
-var app = document.getElementById('app')
-var wrap = document.getElementsByClassName('wrap-p')[0]
+function route(){
+    var app = document.getElementById('app')
+    var wrap = document.getElementsByClassName('wrap-p')[0]
 
- //获取路由#
-let hash = location.hash.substr(1) || 1 //给个默认路由
-
-//获取界面
-var p = document.querySelector(`.p${hash}`)
-
-//路由是否存在
-if(!p){
-    p = document.querySelector('.p404')
-}
-
-//更新dom
-app.appendChild(p)
-
-
-//监听hash改变
-window.addEventListener('hashchange',function(){
     //获取路由#
-    let hash = location.hash.substr(1)
+    let hash = location.hash.substr(1) || 1 //给个默认路由
 
     //获取界面
     var p = document.querySelector(`.p${hash}`)
-    
+
+    if(app.children.length>0){
+        wrap.appendChild(app.children[0])
+    }
 
     //路由是否存在
-    
-    if(p){
-        //更新dom
-        wrap.appendChild(app.children[0]) //把东西再放回去 不能删除了
-        app.appendChild(p)
-    }else{
-        wrap.appendChild(app.children[0])
-        let p404 = document.querySelector('.p404')
-        app.appendChild(p404)
+    if(!p){
+        p = document.querySelector('.p404')
     }
+
+    //更新dom
+    app.appendChild(p)
+}
+
+route()
+
+//监听hash改变
+window.addEventListener('hashchange',function(){
+   route()
 })
