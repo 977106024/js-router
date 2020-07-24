44fdacfa8f9c45c00bf5cb7e04e64193ee511375
bohong <977106024@qq.com>
2020-07-24 18:15:53 +0800

    代码优化

--- router.js
+++ router.js
@@ -8,14 +8,12 @@ let hash = location.hash.substr(1) || 1 //给个默认路由
 var p = document.querySelector(`.p${hash}`)
 
 //路由是否存在
-if(p){
-    //更新dom
-    app.appendChild(p)
-}else{
-    let p404 = document.querySelector('.p404')
-    console.log(p404)
-    app.appendChild(p404)
+if(!p){
+    p = document.querySelector('.p404')
 }
+
+//更新dom
+app.appendChild(p)
 
 
 //监听hash改变
@@ -28,13 +26,14 @@ window.addEventListener('hashchange',function(){
     
 
     //路由是否存在
+    
     if(p){
         //更新dom
         wrap.appendChild(app.children[0]) //把东西再放回去 不能删除了
         app.appendChild(p)
     }else{
+        wrap.appendChild(app.children[0])
         let p404 = document.querySelector('.p404')
-        console.log(p404)
         app.appendChild(p404)
     }
 })
